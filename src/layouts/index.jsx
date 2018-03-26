9bd77f50ec9b8783729990536f9f0d54526df558
Marc-Antoine Ruel <hello@marcantoineruel.com>
2018-03-26 17:17:53 -0400

    import font and normalize.css

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import WebFont from 'webfontloader';
+
+import 'normalize.css';
+
+import './../assets/fonts/bebasNeue/bebasNeue.css';
 
 import './../assets/styles/styles.scss';
+
+WebFont.load({
+  google: {
+    families: ['Cousine'],
+  },
+  // custom: {
+  //   families: ['Bebas Neue'],
+  //   urls: ['./../assets/fonts/bebasNeue/bebasNeue.css'],
+  // }
+});
 
 const TemplateWrapper = ({ children }) => (
   <div>
