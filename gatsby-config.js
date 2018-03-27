8a9257ed6641f62ef44ec76a2030d26fc5985313
Marc-Antoine Ruel <hello@marcantoineruel.com>
2018-03-27 11:03:04 -0400

    repair font loading issue by using gatsby-plugin-google-fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,14 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     {
+      resolve: 'gatsby-plugin-google-fonts',
+      options: {
+        fonts: [
+          'Cousine',
+        ],
+      },
+    },
+    {
       resolve: 'gatsby-plugin-postcss-sass',
       options: {
         postCssPlugins: [autoprefixer()],
