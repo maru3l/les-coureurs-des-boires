3cbf15d0e09e854674931a1c1433c78d9781e1ed
Marc-Antoine Ruel <hello@marcantoineruel.com>
2018-03-27 17:30:28 -0400

    create MenuNavigation component

---
+++ src/components/SiteHeader/SiteMenu/MenuNavigation/index.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Link from 'gatsby-link';
+
+import './style.scss';
+
+const ListItemLink = ({ to, children }) => (
+  <li>
+    <Link
+      to={to}
+      className="site-navigation__link"
+      activeClassName="site-navigation__link--is-active"
+    >
+      {children}
+    </Link>
+  </li>
+);
+
+export default () => (
+  <nav className="site-navigation">
+    <ul>
+      <ListItemLink to="voyages">Voyages</ListItemLink>
+      <ListItemLink to="editoriaux">Éditoriaux</ListItemLink>
+      <ListItemLink to="degustations">Dégustations</ListItemLink>
+      <ListItemLink to="livres">Livres</ListItemLink>
+      <ListItemLink to="conferences">Conférences</ListItemLink>
+      <ListItemLink to="les-coureurs">Les coureurs</ListItemLink>
+    </ul>
+  </nav>
+);
