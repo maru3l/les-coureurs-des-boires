e228b96641164c76954a1d420c2cec7ce8e1bf1e
Marc-Antoine Ruel <hello@marcantoineruel.com>
2018-03-27 17:32:25 -0400

    add fake content to index page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,5 +3,45 @@ import React from 'react';
 export default () => (
   <div>
     <h1>Les coureurs des boires</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia illum nemo consectetur, dolor dicta expedita quos similique, tempore eaque laboriosam tenetur veniam aperiam nisi eligendi fugit eum omnis blanditiis ex.
+    </p>
+    <p>Accusamus iusto quidem ab aliquam assumenda est pariatur corporis voluptatem nobis esse repudiandae placeat debitis rem fugiat, perferendis ullam illum similique nam aut voluptatibus, corrupti, quisquam iste! Expedita, repellendus natus.
+    </p>
+    <p>Modi nobis, placeat quae iste quos aspernatur quidem. Cum odit obcaecati quos iste blanditiis, rerum a minima neque numquam suscipit non iusto atque vero nobis et ad? Esse, aliquid, libero.
+    </p>
+    <p>Est pariatur, ipsum sequi fuga ab iusto, dicta, qui dolorum autem quia, omnis consectetur. Nemo obcaecati amet sequi inventore iste voluptas dicta et ipsa asperiores soluta, ut earum, vitae debitis!
+    </p>
+    <p>Mollitia iste iure alias accusantium dicta in molestiae provident vero, vitae natus obcaecati maxime, fuga ab, nesciunt tenetur tempora suscipit dolores, perferendis magni animi eveniet voluptate commodi eos. Ad, repellat.
+    </p>
+    <p>Voluptatem eos voluptatum at suscipit dolorem. Eligendi in ea ab sapiente quibusdam esse quasi ex, illo temporibus accusantium perspiciatis omnis nisi saepe cum amet aspernatur non inventore quam neque est!
+    </p>
+    <p>Commodi, praesentium alias non corporis eaque ut! Et praesentium fugiat a rem soluta aliquam voluptas voluptatum, non mollitia eveniet incidunt dicta nisi, voluptatem sapiente veniam, similique delectus saepe nesciunt aut!
+    </p>
+    <p>Neque, officiis quae quisquam iusto ea repudiandae repellendus totam? At, ab quaerat facilis sequi molestiae vero cupiditate nihil, modi necessitatibus aliquid sunt aliquam ipsam et voluptas, praesentium atque quasi beatae.
+    </p>
+    <p>Delectus, maiores. Nemo commodi nam labore impedit pariatur quidem hic, possimus, necessitatibus officiis velit harum. Dolorum porro in praesentium magni voluptatem voluptatum sequi? Tenetur eaque, reprehenderit ratione deleniti quia suscipit!
+    </p>
+    <p>Iste praesentium enim beatae saepe nemo accusamus ea fugit, tenetur nobis eum voluptatem esse cum in expedita sit unde doloribus dolorum ab consequuntur harum vitae laboriosam. Suscipit dolore ad eos!
+    </p>
+    <p>Veritatis, eaque, dolorem. Praesentium et ipsam vel, rerum nisi iusto porro. Dolorem dolorum sequi, asperiores ducimus necessitatibus! Rem molestiae ipsum iure voluptatum, accusamus ducimus delectus, vero facere, nesciunt deserunt dicta.
+    </p>
+    <p>Aliquam ea aut non eaque nihil, vero voluptates ipsa. Ea doloremque enim eaque praesentium ex alias. Autem sunt optio, alias nulla quisquam animi deserunt tenetur ipsa, saepe eum culpa, eligendi.
+    </p>
+    <p>Sint in nemo libero sequi nostrum excepturi dicta, architecto aperiam numquam eos quod itaque magnam voluptas eligendi consectetur earum ducimus odit, distinctio unde facilis tenetur vero cum dolorem veritatis enim.
+    </p>
+    <p>Soluta qui rem beatae, provident esse et quia maiores distinctio temporibus ratione, voluptates non ea quam cum! Est voluptas numquam molestias esse, nemo voluptatibus ratione, deleniti, natus aperiam sapiente dolorum!
+    </p>
+    <p>Eum, nihil, laudantium. Rerum eligendi, fugiat porro eaque officiis, quaerat distinctio ratione recusandae facere, maxime impedit odit expedita et necessitatibus harum, temporibus. Corrupti velit atque natus dolorem sapiente voluptate autem!
+    </p>
+    <p>Labore tempora totam ut iste voluptas quisquam quas. Obcaecati excepturi, quam minus temporibus deserunt ex labore suscipit dolorum repellat recusandae facilis inventore doloribus, odio ullam a earum enim fugit? Eligendi.
+    </p>
+    <p>Praesentium voluptatum, id, officiis eos in vel porro voluptate eligendi! Provident tempora a ut, maiores ipsum praesentium repudiandae ratione dolor voluptatem. Ipsa, mollitia debitis doloremque. Nam voluptatum, atque reiciendis soluta?
+    </p>
+    <p>Dolor voluptatibus quidem, cupiditate minima deleniti! Fugit vel rem iure expedita at sit explicabo, veniam, incidunt ab vero laudantium accusantium reprehenderit odio eius vitae hic! Excepturi corporis tempora, asperiores laudantium.
+    </p>
+    <p>Rem magnam eum ratione quaerat cum, perferendis reprehenderit voluptate similique itaque distinctio laudantium repudiandae possimus corporis velit ipsum officia iusto aperiam quasi deleniti! Sequi perferendis numquam, libero recusandae voluptas qui!
+    </p>
+    <p>Quo accusantium nobis at facere saepe possimus, quasi aut quam vel et dolores dolorum. Ipsum reprehenderit cupiditate, aliquam, amet quisquam ratione officia minus, libero corrupti provident eligendi facere sint possimus.
+    </p>
   </div>
 );
