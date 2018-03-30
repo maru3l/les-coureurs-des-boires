e3ba4ce1a4b798533ec742bf3158235bb589fb1f
Marc-Antoine Ruel <hello@marcantoineruel.com>
2018-03-30 14:48:55 -0400

    create social menu component

---
+++ src/components/SiteHeader/SiteMenu/MenuSocial/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './style.scss';
+
+export default () => (
+  <ul className="menu-social">
+    <li className="menu-social__list-item">
+      <button className="menu-social__link">Courriel</button>
+    </li>
+    <li className="menu-social__list-item">
+      <a href="https://www.facebook.com/lescoureursdesboires/" className="menu-social__link" target="_blank" rel="noopener noreferrer">Facebook</a>
+    </li>
+    <li className="menu-social__list-item">
+      <a href="https://www.instagram.com/lescoureursdesboires/" className="menu-social__link" target="_blank" rel="noopener noreferrer">Instagram</a>
+    </li>
+  </ul>
+);
