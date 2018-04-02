da04a745350cebee982bf358b12b0f89401e8569
Marc-Antoine Ruel <hello@marcantoineruel.com>
2018-04-02 17:50:02 -0400

    add search icon in menu-handle

--- src/components/SiteHeader/MenuHandle/index.jsx
+++ src/components/SiteHeader/MenuHandle/index.jsx
@@ -19,13 +19,18 @@ const MenuHandle = (props) => {
         onClick={() => props.onClick()}
       />
 
-      <div className="menu-handle__follow">
+      <button className="menu-handle__follow" onClick={() => props.onClick()}>
         <p>Suivez les coureurs</p>
-      </div>
+      </button>
 
-      <div className="menu-handle__search">
-        <p>S</p>
-      </div>
+      <button className="menu-handle__search" onClick={() => props.onClick()}>
+        <svg xmlns="http://www.w3.org/2000/svg" width="36" height="33" viewBox="0 0 36 33">
+          <g fill="none" fillRule="evenodd" stroke="#002537" transform="rotate(-45 16.45 6.964)">
+            <circle cx="11.207" cy="11.379" r="10.5" />
+            <path strokeLinecap="square" d="M10.7071068,24.8786797 L10.7071068,33.8786797" />
+          </g>
+        </svg>
+      </button>
     </div>
   );
 };
