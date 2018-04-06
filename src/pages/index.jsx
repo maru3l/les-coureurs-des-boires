7a577d4500ba35a860ce12fd51c47d63a613b544
Marc-Antoine Ruel <hello@marcantoineruel.com>
2018-04-06 12:25:58 -0400

    add article list to home page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,13 +1,83 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-export default () => (
-  <div>
-    <div style={{ height: '60vh', backgroundColor: 'white' }} />
-    <h1>Les coureurs des boires</h1>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia illum
-      nemo consectetur, dolor dicta expedita quos similique, tempore eaque
-      laboriosam tenetur veniam aperiam nisi eligendi fugit eum omnis blanditiis ex.
-    </p>
-  </div>
-);
+import ArticleList from '../components/ArticleList';
+
+class IndexPage extends Component {
+  constructor(props) {
+    super(props);
+
+    const { data } = props;
+
+    this.state = {
+      articles: data.allContentfulArticle.edges,
+    };
+  }
+  getArticles() {
+    return this.state.articles.map(({ node }) => ({
+      thumbnail: {
+        src: node.thumbnail.file.url,
+        alt: node.thumbnail.description,
+      },
+      date: node.createdAt,
+      title: node.title,
+      excerpt: node.body.childMarkdownRemark.excerpt,
+      id: node.id,
+      path: node.fields.path,
+    }));
+  }
+
+  render() {
+    return (
+      <div>
+        <div style={{ height: '60vh', backgroundColor: 'white' }} />
+        <h1>Les coureurs des boires</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia illum
+          nemo consectetur, dolor dicta expedita quos similique, tempore eaque
+          laboriosam tenetur veniam aperiam nisi eligendi fugit eum omnis blanditiis ex.
+        </p>
+        <ArticleList articles={this.getArticles()} />
+      </div>
+    );
+  }
+}
+
+export const query = graphql`
+  query IndexQuery {
+    allContentfulArticle(sort: {order: DESC, fields: [createdAt]}) {
+      edges {
+        node {
+          id
+          title
+          thumbnail {
+            file {
+              url
+            }
+            description
+          }
+          body {
+            id
+            childMarkdownRemark {
+              excerpt
+            }
+          }
+          createdAt(formatString: "DD/MM/YYYY")
+          fields {
+            path
+          }
+        }
+      }
+    }
+  }
+`;
+
+IndexPage.propTypes = {
+  data: PropTypes.shape({
+    allMarkdownRemark: PropTypes.shape({
+      edges: PropTypes.array,
+    }),
+  }).isRequired,
+};
+
+export default IndexPage;
