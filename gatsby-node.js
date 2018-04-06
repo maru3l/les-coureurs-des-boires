f7e70e515dbb018a510bd45202dde1e1ec563480
Marc-Antoine Ruel <hello@marcantoineruel.com>
2018-04-06 12:25:06 -0400

    Create path and page for every article from contentful

---
+++ gatsby-node.js
@@ -0,0 +1,79 @@
+const path = require('path');
+
+exports.onCreateNode = ({ node, boundActionCreators }) => {
+  const { createNodeField } = boundActionCreators;
+  if (node.internal.type === 'ContentfulArticle') {
+    let slugPath = '';
+
+    switch (node.category) {
+      case 'Éditorial':
+        slugPath = '/editoriaux/';
+        break;
+      case 'Voyage':
+        slugPath = '/voyages/';
+        break;
+      case 'Dégustation':
+        slugPath = '/degustations/';
+        break;
+      default:
+        slugPath = '/';
+    }
+
+    createNodeField({
+      node,
+      name: 'path',
+      value: `${slugPath}${node.slug}/`,
+    });
+  }
+};
+
+exports.createPages = ({ graphql, boundActionCreators }) => {
+  const { createPage } = boundActionCreators;
+  return new Promise((resolve, reject) => {
+    graphql(`
+      {
+        allContentfulArticle {
+          edges {
+            node {
+              id
+              tags
+              fields {
+                path
+              }
+            }
+          }
+        }
+      }
+    `)
+      .then((result) => {
+        // Generate Article page
+        result.data.allContentfulArticle.edges.forEach(({ node }) => {
+          createPage({
+            path: node.fields.path,
+            component: path.resolve('./src/templates/article.jsx'),
+            context: {
+              id: node.id,
+            },
+          });
+        });
+
+        // Generate tags page
+        const tags = result.data.allContentfulArticle.edges
+          .reduce((acc, { node }) => [...acc, ...node.tags], [])
+          .filter((el, i, self) => i === self.indexOf(el));
+
+        tags.forEach((tag) => {
+          createPage({
+            path: `/tags/${tag}`,
+            component: path.resolve('./src/templates/tagPage.jsx'),
+            context: { tag },
+          });
+        });
+
+        resolve();
+      })
+      .catch((e) => {
+        reject(e);
+      });
+  });
+};
