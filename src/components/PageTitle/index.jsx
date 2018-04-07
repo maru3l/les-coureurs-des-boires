fe328bd68e540accc0623512a0fd490b0e9ea7a8
Marc-Antoine Ruel <hello@marcantoineruel.com>
2018-04-07 11:18:52 -0400

    add line in front of page title and add sub title

--- src/components/PageTitle/index.jsx
+++ src/components/PageTitle/index.jsx
@@ -3,16 +3,23 @@ import PropTypes from 'prop-types';
 
 import './style.scss';
 
-const PageTitle = ({ title }) => (
+const PageTitle = ({ title, subTitle }) => (
   <div className="page-title">
     <div className="page-title__content">
       <p className="page-title__title">{title}</p>
+
+      <p className="page-title__sub-title">{subTitle}</p>
     </div>
   </div>
 );
 
 PageTitle.propTypes = {
   title: PropTypes.string.isRequired,
+  subTitle: PropTypes.string,
+};
+
+PageTitle.defaultProps = {
+  subTitle: null,
 };
 
 export default PageTitle;
