663e701fec3a58bfe2d060190ba4d1dab4f4994e
Marc-Antoine Ruel <hello@marcantoineruel.com>
2018-04-08 20:16:42 -0400

    Add BookList to livres page

--- src/pages/livres/index.jsx
+++ src/pages/livres/index.jsx
@@ -1,9 +1,47 @@
 import React from 'react';
 
 import PageTitle from '../../components/PageTitle';
+import BookList from '../../components/BookList';
 
-export default () => (
-  <React.Fragment>
-    <PageTitle title="livres" />
-  </React.Fragment>
-);
+const PageLivre = ({ data }) => {
+  const { books, site } = data;
+  console.log(data);
+  return (
+    <React.Fragment>
+      <PageTitle title="livres" />
+      <BookList books={books.edges} />
+    </React.Fragment>
+  );
+};
+
+export const query = graphql`
+  query BooksPageQuery {
+    site {
+      siteMetadata {
+        title
+        siteUrl
+      }
+    }
+    books:allContentfulLivre(sort: {order: DESC, fields: [publicationDate]}) {
+      edges {
+        node {
+          id
+          title
+          cover {
+            sizes(maxWidth: 304) {
+              ...GatsbyContentfulSizes_withWebp
+            }
+          }
+          body {
+            childMarkdownRemark {
+              html
+            }
+          }
+          video
+        }
+      }
+    }
+  }
+`;
+
+export default PageLivre;
