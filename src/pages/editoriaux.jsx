1f21cad216cb4e76b59e52b3e11c1c05708ab9a5
Marc-Antoine Ruel <hello@marcantoineruel.com>
2018-04-09 21:35:20 -0400

    Create éditoriaux page

---
+++ src/pages/editoriaux.jsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import PageTitle from '../components/PageTitle';
+import ArticleList from '../components/ArticleList';
+
+const EditoriauxPage = ({ data }) => {
+  const articles = data.articles.edges.map(({ node }) => ({
+    thumbnail: {
+      sizes: node.hero.sizes,
+      alt: node.hero.description,
+    },
+    date: node.publicationDate,
+    title: node.title,
+    excerpt: node.description.description,
+    id: node.id,
+    path: node.fields.path,
+  }));
+
+  const style = {};
+
+  style.title = {
+    border: '0',
+    clip: 'rect(0 0 0 0)',
+    height: '1px',
+    margin: '-1px',
+    overflow: 'hidden',
+    padding: '0',
+    position: 'absolute',
+    width: '1px',
+  };
+
+  return (
+    <React.Fragment>
+      <h1 style={style.title}>Éditoriaux</h1>
+      <PageTitle title="Éditoriaux" />
+      <ArticleList articles={articles} />
+    </React.Fragment>
+  );
+};
+
+export const query = graphql`
+  query EditoriauxQuery {
+    articles:allContentfulArticle(
+      sort: {order: DESC, fields: [publicationDate]},
+      filter: {category: {eq: "Éditorial"}}) {
+      edges {
+        node {
+          id
+          title
+          publicationDate(formatString: "DD/MM/YYYY")
+          hero {
+            sizes(maxWidth: 304) {
+              ...GatsbyContentfulSizes_withWebp
+            }
+            description
+          }
+          description {
+            description
+          }
+          fields {
+            path
+          }
+        }
+      }
+    }
+  }
+`;
+
+EditoriauxPage.propTypes = {
+  data: PropTypes.shape({
+    allMarkdownRemark: PropTypes.shape({
+      edges: PropTypes.array,
+    }),
+  }).isRequired,
+};
+
+export default EditoriauxPage;
