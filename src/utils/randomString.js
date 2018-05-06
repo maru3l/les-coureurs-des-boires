dad8bd6dd3d83d5db45cd6e828304a89056fb118
Marc-Antoine Ruel <hello@marcantoineruel.com>
2018-05-06 10:26:01 -0400

    create randomString util

---
+++ src/utils/randomString.js
@@ -0,0 +1,11 @@
+export default (len = 32) => {
+  let outStr = '';
+  let newStr = '';
+  while (outStr.length < len) {
+    newStr = Math.random()
+      .toString(36)
+      .slice(2);
+    outStr += newStr.slice(0, Math.min(newStr.length, len - outStr.length));
+  }
+  return outStr;
+};
