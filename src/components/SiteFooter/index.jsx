119f59bd7e303beefbbeb7b9f2b9026f658844b0
Marc-Antoine Ruel <hello@marcantoineruel.com>
2018-05-24 13:11:31 -0400

    add copyright in footer

--- src/components/SiteFooter/index.jsx
+++ src/components/SiteFooter/index.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 
 import './style.scss';
 
+const getYears = () => new Date().getFullYear();
+
 export default () => (
-  <footer className="site-footer" />
+  <footer className="site-footer">
+    <p>© {getYears()} - Les coureurs des boires</p>
+  </footer>
 );
