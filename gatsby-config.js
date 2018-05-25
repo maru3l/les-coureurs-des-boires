3a0a8b0821b558ae7f453e00d846bcce9c87934d
Marc-Antoine Ruel <hello@marcantoineruel.com>
2018-05-25 11:46:38 -0400

    add rss feed

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,17 +4,20 @@ require('dotenv').config();
 module.exports = {
   siteMetadata: {
     title: 'Les coureurs des boires',
+    description:
+      "Présentation des bières Coureurs des Boires et d'autres moments de dégustations notables en lien avec les travaux du duo",
     siteUrl: 'https://www.lescoureursdesboires.com',
   },
   plugins: [
-    'gatsby-plugin-react-helmet',
     'gatsby-plugin-react-next',
+    'gatsby-plugin-react-helmet',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-remark',
     {
       resolve: 'gatsby-plugin-google-fonts',
       options: {
-        fonts: [
-          'Cousine',
-        ],
+        fonts: ['Cousine'],
       },
     },
     {
@@ -38,9 +41,64 @@ module.exports = {
         path: `${__dirname}/src/assets/imgs/`,
       },
     },
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp',
-    'gatsby-transformer-remark',
+    {
+      resolve: 'gatsby-plugin-feed',
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allContentfulArticle } }) =>
+              allContentfulArticle.edges.map(edge =>
+                Object.assign(
+                  {},
+                  {
+                    title: edge.node.title,
+                    date: edge.node.publicationDate,
+                    description: edge.node.body.childMarkdownRemark.excerpt,
+                    url: site.siteMetadata.siteUrl + edge.node.fields.path,
+                    guid: site.siteMetadata.siteUrl + edge.node.fields.path,
+                  },
+                )),
+            query: `{
+              site {
+                siteMetadata {
+                  title
+                  siteUrl
+                }
+              }
+              allContentfulArticle(sort: { order: DESC, fields: [publicationDate] }) {
+                edges {
+                  node {
+                    title
+                    publicationDate
+                    body {
+                      childMarkdownRemark {
+                        excerpt
+                      }
+                    }
+                    fields {
+                      path
+                    }
+                  }
+                }
+              }
+            }`,
+            output: '/rss.xml',
+          },
+        ],
+      },
+    },
     'gatsby-plugin-sitemap',
     'gatsby-plugin-netlify',
   ],
