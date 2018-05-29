bdd5a4dbe1494f648cdbd44e7fd5572ef715828e
Marc-Antoine Ruel <hello@marcantoineruel.com>
2018-05-29 12:16:02 -0400

    add favicon

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -15,12 +15,41 @@ const TemplateWrapper = ({ children }) => (
   <React.Fragment>
     <Helmet>
       <html lang="fr" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png?v=A0ePJoooGx"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png?v=A0ePJoooGx"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png?v=A0ePJoooGx"
+      />
+      <link rel="manifest" href="/site.webmanifest?v=A0ePJoooGx" />
+      <link
+        rel="mask-icon"
+        href="/safari-pinned-tab.svg?v=A0ePJoooGx"
+        color="#002537"
+      />
+      <link rel="shortcut icon" href="/favicon.ico?v=A0ePJoooGx" />
+      <meta
+        name="apple-mobile-web-app-title"
+        content="Les coureurs des boires"
+      />
+      <meta name="application-name" content="Les coureurs des boires" />
+      <meta name="msapplication-TileColor" content="#0b2536" />
+      <meta name="theme-color" content="#fff9ec" />
     </Helmet>
     <Header />
 
-    <main>
-      { children() }
-    </main>
+    <main>{children()}</main>
 
     <Footer />
   </React.Fragment>
